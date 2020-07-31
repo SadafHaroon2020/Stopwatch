102cca9829bcfbcbe3e3645e9e41b34a852cb30a
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-07-30 23:46:02 -0500

    Creating Stopwatch.js

---
+++ src/Components/Stopwatch.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Stopwatch extends React.Component {
+  
+  render() {
+
+    return (
+      <div className="Stopwatch">
+        <div><h3>Stopwatch</h3></div>
+        
+        
+      </div>
+    );
+  }
+}
+export default Stopwatch;
