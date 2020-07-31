b8dd34ffd166e4a36f2fb25631e4f23a006531de
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-07-31 00:31:49 -0500

    Defining states and functions body with elemnets

--- src/Components/Stopwatch.js
+++ src/Components/Stopwatch.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 class Stopwatch extends React.Component {
+  /*Defining States for stopwatch intially set isTimerOn : false as start state.*/
+  state = {
+    isTimerOn: false,
+    timer_start: 0,
+    timerTime: 0
+  };
+ /**
+  * Defining on click function which will call on related button 
+  */
+  start = () => {
   
+  };
+  stop = () => {
+
+  };
+
+
   render() {
 
     return (
       <div className="Stopwatch">
         <div><h3>Stopwatch</h3></div>
-        
-        
+        <div className="display">
+   
+            <button onClick="">Start</button>
+            <button onClick="">Stop</button>
+            <button onClick="">Pause</button>
+            <button onClick="">Restart</button>
+         
+        </div>
       </div>
     );
   }
